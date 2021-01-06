88da2f641af6d877dd6ecee72420260f9cac7b3f
fabiana.zabala <fabianazabala@hotmail.es>
2021-01-06 02:02:15 +0100

    Added descriptions to TimerCounter class and to its public methods.

--- src/main/java/com/epam/prejap/tetris/game/TimeCounter.java
+++ src/main/java/com/epam/prejap/tetris/game/TimeCounter.java
@@ -3,6 +3,10 @@ package com.epam.prejap.tetris.game;
 import java.time.Duration;
 import java.time.Instant;
 
+/**
+ * TimeCounter class calculates the elapsed time in every cycle of the game using the hh:mm:ss format.
+ * @author Fabiana Zabala
+ */
 public class TimeCounter {
 
   private final Instant startTime;
@@ -11,10 +15,18 @@ public class TimeCounter {
     this.startTime = startTime;
   }
 
+  /**
+   * Creates a time counter initializing {@link TimeCounter#startTime} to current time.
+   * @return TimeCounter
+   */
   public static TimeCounter createStart() {
     return new TimeCounter(Instant.now());
   }
 
+  /**
+   * Returns time passed between {@link TimeCounter#startTime} and current time.
+   * @return String in format hh:mm:ss
+   */
   public String totalDuration(){
     var timePassed = getTimePassed();
     return String.format("%02d:%02d:%02d",
